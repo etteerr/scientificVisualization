6a033bf44d5711f0d990dedc1457082191da0fa6
Erwin Diepgrond <e.j.diepgrond@gmail.com>
2017-10-14 13:29:44 +0200

    Cleaned vtk_app

--- SVVR_Flow/SVVR_Flow/vtk_app.cpp
+++ SVVR_Flow/SVVR_Flow/vtk_app.cpp
@@ -24,42 +24,52 @@ int loadDatasets(std::map<std::string, std::string> & arguments)
 
 	/* Load files specified by arguments */
 	for (unsigned int i = start; i < end; i += step) {
-		/* Create reader*/
-		vtkSmartPointer<vtkStructuredPointsReader> reader = vtkSmartPointer<vtkStructuredPointsReader>::New();
-
 		/* Create filename */
 		char endfilename[4096];
 		sprintf_s(endfilename, 4096, filename.c_str(), i);
 
 		/* Create full path*/
-		std::string fn;
+		std::string fn; //filename (full path)
 		fn.append(arguments["-d"]);
 		fn.append(endfilename);
 
-		/* Verbosity */
-		dprintf(dbglvl_NOTIFICATION, "loading %s...\n", fn.c_str());
-
-		/* vtk load dataset*/
-		try {
-			reader->SetFileName(fn.c_str());
-			reader->Update();
-		}
-		catch (std::exception e) {
-			dprintf(dbglvl_ERROR, "Exception while loading %s!\n", fn.c_str());
-			dprintf(dbglvl_ERROR, "Reason: %s\n", e.what());
-			return 1;
-		}
-
-		/* Check if loading was successfull */
-		if (reader->GetErrorCode() != NOERROR) {
-			dprintf(dbglvl_ERROR, "Error while loading %s! (code: %i)\n", fn.c_str(), reader->GetErrorCode());
-			dprintf(dbglvl_ERROR, "\tError: %s\n",  vtkErrorCode::GetStringFromErrorCode(reader->GetErrorCode()));
-			return 1;
-		}
-
-		/* Save to dataset */
-		dataset[numData++] = reader;
+		/* Load pointset data */
+		int retval = LoadPointsSet(fn);
+		if (retval < 0) return 1;
 	}
 
 	return 0;
+}
+
+
+int LoadPointsSet(std::string &fn)
+{
+	/* Verbosity */
+	dprintf(dbglvl_NOTIFICATION, "loading %s...\n", fn.c_str());
+
+	/* Create reader*/
+	vtkSmartPointer<vtkStructuredPointsReader> reader = vtkSmartPointer<vtkStructuredPointsReader>::New();
+
+	/* vtk load dataset*/
+	try {
+		reader->SetFileName(fn.c_str());
+		reader->Update();
+	}
+	catch (std::exception e) {
+		dprintf(dbglvl_ERROR, "Exception while loading %s!\n", fn.c_str());
+		dprintf(dbglvl_ERROR, "Reason: %s\n", e.what());
+		return -1;
+	}
+
+	/* Check if loading was successfull */
+	if (reader->GetErrorCode() != NOERROR) {
+		dprintf(dbglvl_ERROR, "Error while loading %s! (code: %i)\n", fn.c_str(), reader->GetErrorCode());
+		dprintf(dbglvl_ERROR, "\tError: %s\n", vtkErrorCode::GetStringFromErrorCode(reader->GetErrorCode()));
+		return -1;
+	}
+
+	/* Save to dataset */
+	dataset[numData++] = reader;
+
+	return numData-1;
 }
--- SVVR_Flow/SVVR_Flow/vtk_app.h
+++ SVVR_Flow/SVVR_Flow/vtk_app.h
@@ -3,3 +3,5 @@
 int entry(std::map<std::string, std::string>& arguments);
 
 int loadDatasets(std::map<std::string, std::string> & arguments);
+
+int LoadPointsSet(std::string &fn);
